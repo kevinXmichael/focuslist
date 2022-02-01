9ec976c093baa126dad5bdc3a37bb2f25ef0c35a
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-01 21:10:22 +0100

    added i18n

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,22 @@
 <script>
   import "@/styles/app.scss"
+  import "@/i18n"
 
   import Fa from "svelte-fa"
   import { faPlusSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons"
+
+  import { _, isLoading } from "svelte-i18n"
 </script>
 
-<main
-  class="p-sm m-auto w-full h-full flex flex-col items-center justify-center"
->
-  <h1>FocusList</h1>
-  <div class="text-white">
-    <Fa icon={faPlusSquare} class="std-hover" size="sm" /> Add
-    <Fa icon={faTrashAlt} size="lg" /> Remove
-  </div>
-</main>
+{#if !$isLoading}
+  <main
+    class="p-sm m-auto w-full h-full flex flex-col items-center justify-center"
+  >
+    <h1>{$_("title")}</h1>
+
+    <div class="text-white">
+      <Fa icon={faPlusSquare} class="std-hover" size="sm" /> Add
+      <Fa icon={faTrashAlt} size="lg" /> Remove
+    </div>
+  </main>
+{/if}
---
+++ src/i18n/index.js
@@ -0,0 +1,8 @@
+import { init, register } from "svelte-i18n"
+
+register("en", () => import("./en.json"))
+
+init({
+  fallbackLocale: "en",
+  initialLocale: "en"
+})
